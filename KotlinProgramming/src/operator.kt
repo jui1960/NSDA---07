3a8b89fad4af5a522adfb36f76943d1e6d36df78
jui1960 <juifull196044@gmail.com>
2026-01-06 17:36:19 +0600

    comparison operator

--- KotlinProgramming/src/operator.kt
+++ KotlinProgramming/src/operator.kt
@@ -27,46 +27,35 @@ logical operator a&&b a||b a!b
 //}
 
 //Arithmetic operator
-//fun main() {
-//    print("Enter your number of a : ")
-//    var a = readln().toInt()
-//    print("Enter your number of a : ")
-//    var b = readln().toInt()
-//
-//    val sum = a+b
-//    val sub = a-b
-//    val mul = a*b
-//    val div = a/b
-//    val mod = a%b
-//
-//    println("Addition of a = $a and b = $b is = $sum")
-//    println("Substraction of a = $a and b = $b is = $sub")
-//    println("Multiplication of a = $a and b = $b is = $mul")
-//    println("Division of a = $a and b = $b is = $div")
-//    println("Modulation of a = $a and b = $b is = $mod")
-//    a++
-//    b--
-//    println("Increment value of a is = $a")
-//    println("Decrement value of b is = $b")
-//}
+fun main() {
+    print("Enter your number of a : ")
+    var a = readln().toInt()
+    print("Enter your number of a : ")
+    var b = readln().toInt()
 
+    val sum = a+b
+    val sub = a-b
+    val mul = a*b
+    val div = a/b
+    val mod = a%b
 
-
-//Assignment operator
-fun main(){
-    var a = 20
-    println("Initial value of a is : $a")
-    a += 10
-    println("Value of a after adding 10 is : $a")
-    a -= 5
-    println("Value of a after subtracting 5 is : $a")
-    a *= 5
-    println("Value of a after multiplying by 5 is : $a")
-    a /= 3
-    println("Value of a after dividing by 3 is : $a")
-    a %= 4
-    println("Value of a after modulus by 4 is : $a")
-
+    println("Addition of a = $a and b = $b is = $sum")
+    println("Substraction of a = $a and b = $b is = $sub")
+    println("Multiplication of a = $a and b = $b is = $mul")
+    println("Division of a = $a and b = $b is = $div")
+    println("Modulation of a = $a and b = $b is = $mod")
+    a++
+    b--
+    println("Increment value of a is = $a")
+    println("Decrement value of b is = $b")
 }
+
+
+    
+
+    
+    
+
+
 
 
