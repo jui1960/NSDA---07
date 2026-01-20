3988dc4cb95a97de467c57bde44b91c11dbc8f1f
jui1960 <juifull196044@gmail.com>
2026-01-20 22:59:52 +0600

    constructorr

--- KotlinProgramming/OOP/src/constructor.kt
+++ KotlinProgramming/OOP/src/constructor.kt
@@ -4,4 +4,13 @@ fun main() {
     val c1 = Car("Ford", "Mustang", 1969)
     val c2 = Car("BMW", "X5", 1999)
     val c3 = Car("Tesla", "Model S", 2020)
+
+
+//
+//class Carr(var brand: String, var model: String, var year: Int)
+//
+//
+//    val c11 = Carr("Ford", "Mustang", 1969)
+//    val c21 = Carr("BMW", "X5", 1999)
+//    val c31 = Carr("Tesla", "Model S", 2020)
 }
