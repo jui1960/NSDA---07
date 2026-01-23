85e49536cfe2971c84fa2e20ee147afad6efff29
jui1960 <juifull196044@gmail.com>
2026-01-23 22:19:29 +0600

    prtected

--- KotlinProgramming/OOP/src/Encapsulation.kt
+++ KotlinProgramming/OOP/src/Encapsulation.kt
@@ -12,20 +12,44 @@ fun main(){
 
 }*/
 
-//
 
+
+
+/*private access modifire
 private open class Namee1{
     val name = "jui"
     fun display(){
         println("my name is $name")
     }
 }
+*/
 /*class name22 : Namee1(){
      namee1 class ta private thakar karone ami name22 theke inherit korte prsi na
-}*/
+}*//*
+
 
 fun main(args: Array<String>){
     val ans = Namee1()
     ans.display()
-    ans.name
+
 }
+*/
+
+
+open  class Prot {
+    protected fun display(){
+        println("protected")
+    }
+}
+class Oop:Prot(){
+    fun Show(){
+        display()
+    }
+
+}
+
+fun main() {
+    val a = Oop()
+    a.Show()
+}
+
