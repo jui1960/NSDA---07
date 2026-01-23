16c940d3486e94047c76d326a6a33409135af79e
jui1960 <juifull196044@gmail.com>
2026-01-23 22:08:38 +0600

    private

--- KotlinProgramming/OOP/src/Encapsulation.kt
+++ KotlinProgramming/OOP/src/Encapsulation.kt
@@ -13,3 +13,19 @@ fun main(){
 }*/
 
 //
+
+private open class Namee1{
+    val name = "jui"
+    fun display(){
+        println("my name is $name")
+    }
+}
+/*class name22 : Namee1(){
+     namee1 class ta private thakar karone ami name22 theke inherit korte prsi na
+}*/
+
+fun main(args: Array<String>){
+    val ans = Namee1()
+    ans.display()
+    ans.name
+}
