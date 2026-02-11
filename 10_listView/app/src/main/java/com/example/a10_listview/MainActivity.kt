e315d2c5a934901cc05c728e24707beb1baec0d9
jui1960 <juifull196044@gmail.com>
2026-02-11 12:39:36 +0600

    update

--- 10_listView/app/src/main/java/com/example/a10_listview/MainActivity.kt
+++ 10_listView/app/src/main/java/com/example/a10_listview/MainActivity.kt
@@ -3,18 +3,36 @@ package com.example.a10_listview
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.a10_listview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var friendlist  : ArrayList<Friend>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadData()
+        val adapter = FriendAdepter(this,friendlist)
+        binding.listview.adapter = adapter
+
+
+
+    }
+    private fun loadData(){
+        friendlist= arrayListOf(
+            Friend("jannati akter jui","he is very good girl",
+                R.drawable.picc),
+            Friend("Aminul Islam Munna", "he is a very good boy he is a very good boy he is a very good boy he is a very good boy he is a very good boy he is a very good boy", R.drawable.picc),
+            Friend("Aminul Islam Munna", "he is a very good boy", R.drawable.picc),
+            Friend("Aminul Islam Munna", "he is a very good boy", R.drawable.picc),
+            Friend("Aminul Islam Munna", "he is a very good boy", R.drawable.picc),
+            Friend("Aminul Islam Munna", "he is a very good boy", R.drawable.picc),
+            Friend("Aminul Islam Munna", "he is a very good boy", R.drawable.picc),
+            Friend("Aminul Islam Munna", "he is a very good boy", R.drawable.picc),
+            Friend("Aminul Islam Munna", "he is a very good boy", R.drawable.picc),
+        )
     }
 }
