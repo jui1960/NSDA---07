603f1b633d2945e903407f0e5290f08dc2b11005
jui1960 <juifull196044@gmail.com>
2026-02-12 15:23:45 +0600

    Recycler View

--- 11_RecyclerView/app/build.gradle.kts
+++ 11_RecyclerView/app/build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
 }
 
 android {
+    buildFeatures{
+        viewBinding = true
+    }
     namespace = "com.example.a11_recyclerview"
     compileSdk = 36
 
--- 11_RecyclerView/app/src/main/java/com/example/a11_recyclerview/MainActivity.kt
+++ 11_RecyclerView/app/src/main/java/com/example/a11_recyclerview/MainActivity.kt
@@ -1,20 +1,38 @@
- package com.example.a11_recyclerview
+package com.example.a11_recyclerview
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.a11_recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var friendAdapter: FriendAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val friendlist = listOf(
+            Friend("Jui", "Very Good Student", R.drawable.picc),
+            Friend("Mahmuda", "Very Good Student", R.drawable.picc),
+            Friend("Nishat", "Very Good Student", R.drawable.picc),
+            Friend("Rownok", "Very Good Student", R.drawable.picc),
+            Friend("Jesi", "Very Good Student", R.drawable.picc),
+            Friend("Jiniiya", "Very Good Student", R.drawable.picc),
+            Friend("Jeri", "Very Good Student", R.drawable.picc),
+
+            )
+        friendAdapter = FriendAdapter(friendlist)
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = friendAdapter
         }
+
     }
 }
