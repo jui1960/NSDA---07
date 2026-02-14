313f5191989e761a810167f3b2641ffb8467828c
jui1960 <juifull196044@gmail.com>
2026-02-14 22:42:22 +0600

    update

--- 12_BMI_Calculator/app/src/main/java/com/example/a12_bmi_calculator/CalculateScreen.kt
+++ 12_BMI_Calculator/app/src/main/java/com/example/a12_bmi_calculator/CalculateScreen.kt
@@ -1,5 +1,6 @@
 package com.example.a12_bmi_calculator
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,8 +18,42 @@ class CalculateScreen : AppCompatActivity() {
 
 
         val username = intent.getStringExtra("USER_NAME")
-
         binding.welcNICal.text = "Hello, $username"
+
+        binding.bmibutton.setOnClickListener {
+
+            val weightv = binding.weightEt.text.toString().trim()
+            val heightv = binding.heightEt.text.toString().trim()
+
+            if (weightv.isEmpty()) {
+                binding.weightlayout.error = "Enter Weight"
+            } else {
+                binding.weightlayout.error = null
+            }
+
+            if (heightv.isEmpty()) {
+                binding.heightlayout.error = "Enter height"
+            } else {
+                binding.heightlayout.error = null
+            }
+
+            val weight = weightv.toDouble()
+            val height = heightv.toDouble()
+
+            val convert = height.toInt()
+            val inc = ((height-convert)*10).toInt()
+            val totalInc = (convert*12)+inc
+
+            val heightt = totalInc*0.0254
+
+            val bmi = weight / (heightt * heightt)
+
+
+            val intent = Intent(this, BmiResult::class.java)
+
+            intent.putExtra("BMI VALUE", bmi)
+            startActivity(intent)
+        }
 
     }
 }
