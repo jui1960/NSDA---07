758359df17f7e9a33da86cb51b5f74b6f0e28917
jui1960 <juifull196044@gmail.com>
2026-02-16 19:01:58 +0600

    sharedPreference

---
+++ 15_SharedPreference_recyclerView/app/src/main/java/com/example/a15_sharedpreference_recyclerview/NoteAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.a15_sharedpreference_recyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.a15_sharedpreference_recyclerview.databinding.ActivityItemNoteBinding
+import com.example.a15_sharedpreference_recyclerview.databinding.ActivityMainBinding
+
+class NoteAdapter(private val notelist: List<Note>) :
+    RecyclerView.Adapter<NoteAdapter.Noteviewholder>() {
+
+    inner class Noteviewholder(val binding: ActivityItemNoteBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): Noteviewholder {
+        val binding =
+            ActivityItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Noteviewholder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: Noteviewholder,
+        position: Int
+    ) {
+        holder.binding.txtItemNote.text = notelist[position].text
+
+    }
+
+    override fun getItemCount(): Int {
+        return notelist.size
+    }
+
+}
