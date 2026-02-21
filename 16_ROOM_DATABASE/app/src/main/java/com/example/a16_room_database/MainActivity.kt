9a7b80c2c45cd054e8d404aef0c0a258220fabd3
jui1960 <juifull196044@gmail.com>
2026-02-21 10:12:17 +0600

    adapter add

--- 16_ROOM_DATABASE/app/src/main/java/com/example/a16_room_database/MainActivity.kt
+++ 16_ROOM_DATABASE/app/src/main/java/com/example/a16_room_database/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.a16_room_database
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.a16_room_database.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeScreen::class.java))
+        },2000)
     }
 }
